b3bc4bd8b81c9be6007527ddc357746a027fafd8
Tari Dekpen <dekpent@gmail.com>
2022-07-23 10:00:16 +0100

    master commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,34 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{log, near_bindgen};
+
+const DEFAULT_MESSAGE: &str = "Hello world";
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    // SETUP CONTRACT STATE
+    message: String,
+}
+
+
+impl Default for Contract{
+
+    fn default() -> Self{
+        Self{message: DEFAULT_MESSAGE.to_string()}
+    }
+
 }
 
 #[near_bindgen]
 impl Contract {
-    // ADD CONTRACT METHODS HERE
-}
-
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+    
+    pub fn get_message(&self) -> String {
+        
+        return self.message.clone();
     }
 
-    // TESTS HERE
+    pub fn set_message(&mut self, message: String) {
+        // Use env::log to record logs permanently to the blockchain!
+        log!("Saving greeting {}", message);
+        self.message = message;
+    }
 }
